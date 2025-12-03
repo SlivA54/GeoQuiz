10cc18d065d0c4f169a8283474f216e5d37c0cd1
SlivA54 <gavrisevdanil765@gmail.com>
2025-12-03 13:08:27 +0700

    Text

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier.Companion
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.geoquiz.ui.theme.GeoQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,3 +64,36 @@ val questions = listOf(
     Question("The Amazon River is the longest river in the Americas.", true),
     Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
 )
+
+@Composable
+fun QuizScreen() {
+    var currentIndex by remember { mutableStateOf(0) }
+    var isAnswered by remember { mutableStateOf(false) }
+    var userScore by remember { mutableStateOf(0) }
+    var isQuizFinished by remember { mutableStateOf(false) }
+
+    val question = questions[currentIndex]
+
+    Column(
+        modifier = Companion
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = question.text,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+
+
+
+        if (isQuizFinished) {
+            Text(
+                text = "Правильных ответов: $userScore из ${questions.size}",
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
